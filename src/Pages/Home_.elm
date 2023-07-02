115b7b09c02b3ff491317d696cb30ff609bebfe7
Daniel Price <64694785+daniel-price@users.noreply.github.com>
2023-07-02 13:12:34 +0100

    Change hompage to element

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,11 +1,72 @@
-module Pages.Home_ exposing (view)
+module Pages.Home_ exposing (Model, Msg, page)
 
-import Html
+import Gen.Params.Home_ exposing (Params)
+import Html exposing (div, text)
+import Page
+import Request
+import Shared
+import Task
 import View exposing (View)
 
 
-view : View msg
-view =
-    { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+page : Shared.Model -> Request.With Params -> Page.With Model Msg
+page _ _ =
+    Page.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}
+    , Task.perform (always NoOp) (Task.succeed ())
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view _ =
+    { title = "Crosswords"
+    , body =
+        [ div [] [ text "Crosswords" ]
+        ]
     }
+
